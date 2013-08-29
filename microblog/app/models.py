07855cbd9afe54299c46923d9a19c18e84c6c696
Aaron Dorheim-Davis <aaron.bdd@gmail.com>
2013-08-29 10:43:16 -0700

    Part V: User Logins http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-v-user-logins

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -9,15 +9,27 @@ class User(db.Model):
     email = db.Column(db.String(120), index = True, unique = True)
     role = db.Column(db.SmallInteger, default = ROLE_USER)
     posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
-    
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+        
+    def get_id(self):
+        return unicode(self.id)
+
     def __repr__(self):
         return '<User {nickname!r}>'.format(nickname = self.nickname)
-        
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    
+
     def __repr__(self):
         return '<Post {body!r}>'.format(body = self.body)
